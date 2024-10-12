6a5dfb7a3f2b1df817e12c41ac352e34d8ed2c15
Mikalai Shalupin <mikalai.shalupin@gmail.com>
2024-10-12 16:38:30 +0300

    feat: start from home directory

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { parseArguments } from "./parseArguments.js";
 import readline from "readline/promises";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { homedir } from "os";
+import { readdir } from "fs/promises";
 
 // npm run start -- --username=ASDFGH
 
@@ -11,8 +13,11 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const home = homedir();
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = home;
 
+process.chdir(home);
 const closeFileManager = () => {
   console.log(`Thank you for using File Manager, ${username}, goodbye!`);
   rl.close();
@@ -25,7 +30,13 @@ rl.on("line", (input) => {
   if (input === ".exit") {
     closeFileManager();
   }
-  console.log(`You are currently in ${__dirname}`);
+  if (input === "ls") {
+    console.log("list");
+    readdir(__dirname).then((data) => {
+      console.log(data);
+    });
+    console.log(`You are currently in ${__dirname}`);
+  }
 });
 
 rl.on("SIGINT", () => {
