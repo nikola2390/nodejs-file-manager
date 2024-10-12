22a8e05ae13627f27da0c5b649717f835b8791c3
Mikalai Shalupin <mikalai.shalupin@gmail.com>
2024-10-12 16:32:51 +0300

    feat: implement current directory message

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { parseArguments } from "./parseArguments.js";
 import readline from "readline/promises";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 // npm run start -- --username=ASDFGH
 
@@ -9,17 +11,21 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const closeFileManager = () => {
   console.log(`Thank you for using File Manager, ${username}, goodbye!`);
   rl.close();
 };
 
 console.log(`Welcome to the File Manager, ${username}!`);
+console.log(`You are currently in ${__dirname}`);
 
 rl.on("line", (input) => {
   if (input === ".exit") {
     closeFileManager();
   }
+  console.log(`You are currently in ${__dirname}`);
 });
 
 rl.on("SIGINT", () => {
