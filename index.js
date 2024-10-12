0162d7f1038cef6a871ff9ef66766b36131ade58
Mikalai Shalupin <mikalai.shalupin@gmail.com>
2024-10-12 16:29:27 +0300

    feat: implement close message

--- index.js
+++ index.js
@@ -9,4 +9,19 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+const closeFileManager = () => {
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+  rl.close();
+};
+
 console.log(`Welcome to the File Manager, ${username}!`);
+
+rl.on("line", (input) => {
+  if (input === ".exit") {
+    closeFileManager();
+  }
+});
+
+rl.on("SIGINT", () => {
+  closeFileManager();
+});
